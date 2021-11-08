34f52ee5da5c0d593f586e7ed98fdf92fa51b856
Maikeru <63614642+maikeru-dev@users.noreply.github.com>
2021-11-08 20:42:55 +0000

    overall performance update
    I've changed a few things...
    
    The game now uses a 2d array to check if a coordinate on the board is taken.
    
    Several comments now exist to explain all difficult concepts, and variable names are far more creative.

--- index.js
+++ index.js
@@ -1,118 +1,121 @@
-var canvas = document.getElementById("myCanvas");
+var canvas = document.getElementById("myCanvas"); // defining essentials.
 var ctx = canvas.getContext("2d");
-var x = 0,y = 0;
-var tickPassed = false;
-var useBrokenNumberGrid = false;
+
+var tickPassed = false; // tick passed is used to gatekeep changing directions after one has already been made.
+var useBrokenNumberGrid = true; // this is a useless variable.
+var board = [] // this is the board, a 2d array in the future that will allow us to quickly find if a coordinate is already occupied.
+
+var x = 0, y = 0;
 ctx.beginPath();
 ctx.font = "15px Verdana";
-for(var i = 0; i < 25;i++){
+
+for (var i = 0; i < 25; i++) { // this generates a grid in the canvas
     ctx.moveTo(x, 0);
     ctx.lineTo(x, 600);
     ctx.moveTo(0, y);
     ctx.lineTo(600, y);
-    y+=25
-    x+=25
+    y += 25
+    x += 25
 }
 ctx.stroke();
-if(useBrokenNumberGrid == true){
-    ctx.fillText(`1`,0,0)
-    for(p = 1; p < 25;p++){
-        for(i = 0; i < 24;i++){
-            ctx.fillText(`${i}`,i*25+3,25*p-3)
+if (useBrokenNumberGrid) { // I have no reason to keep this if statement, but it is a testament to the history of my stupidity
+    for (xAxis = 0; xAxis < 24; xAxis++) {
+        var lane = []
+        for (yAxis = 0; yAxis < 23; yAxis++) {
+            lane[yAxis] = 0 // 0 means nothing, 1 means apple, 2 means body
         }
+        board[xAxis] = lane
     }
 }
-var snake={HeadBlock:{x:300,y:300}, color:"#FF0000", direction:"up", children:[{x:300,y:325}]}
-var point={HeadBlock:{x:300,y:300}, color:"#27d817"}
-function drawBlock(color,block){
+var snake = { HeadBlock: { blockX:12, blockY:12}, color: "#FF0000", direction: "up", children: [{ blockX: 12, blockY: 13}] }
+var point = { HeadBlock: { blockX:0, blockY:0 }, color: "#27d817" }
+
+function drawBlock(color, block) {
     ctx.fillStyle = color;
-    ctx.fillRect(block.x+1,block.y+1,23,23);
+    ctx.fillRect(block.blockX*25 + 1, block.blockY*25 + 1, 23, 23);
 }
-function coordinateToBlock(cx,cy){
-    if(cx == null || cy== null) return false;
-    return {x:cx,y:cy,blockX:cx/25,blockY:cy/25};
+function coordinateToBlock(cx, cy) {
+
+    return { x: cx, y: cy, blockX: cx / 25, blockY: cy / 25 };
 }
-function moveSnake(){
-    drawBlock(snake.color,snake.HeadBlock)
-    if(snake.children.length > 0){
-        const n = snake.children[snake.children.length-1]
-        if(n != snake.children[snake.children.length-2]){
-            drawBlock("#fff",coordinateToBlock(n.x,n.y))
-        }
-       for(var i = snake.children.length-1;i > 0;i--){
-            snake.children[i] = snake.children[i-1]
-        }
-        snake.children[0] = snake.HeadBlock 
-    }else{
-        drawBlock("#fff",coordinateToBlock(snake.HeadBlock.x,snake.HeadBlock.y))
-        
+function checkGameOver() {
+    if (snake.HeadBlock.blockX > 23 || snake.HeadBlock.blockX < 0) {
+        window.open("./index.html", "_self")
+    }else if (snake.HeadBlock.blockY > 23 || snake.HeadBlock.blockY < 0) {
+        window.open("./index.html", "_self")
+    }// basic checks if the snake's head is out of bounds.
+    moveSnake(); // we chain the functions to make sure they run in order, it wouldn't matter if they were run like sub-procedures either, so this is ok.
+}
+
+function moveSnake() {
+    const length = snake.children.length - 1 // This is the index of the last child in the array.
+
+    const cords = snake.children[length] // get the last child in the snake's children array
+    board[cords.blockX][cords.blockY] = 0 // sets the cords on the board to nothing aka 0
+    drawBlock("#fff", cords) // delete the previous child's position to remove marks
+
+    for (var i = length; i > 0; i--) { // This counts down from the array's length, starting from the index of the last child
+        snake.children[i] = snake.children[i - 1] // Example: i = 5: snake.children[5] = snake.children[4], we are shifting, the chain down and eventually the first index is invalid/duplicated.
     }
-    switch(snake.direction){
+    snake.children[0] = snake.HeadBlock // we fix the duplication by setting the current headblock to the first child, which makes a new child and allows us to move the children, along with the head.
+
+
+    var newHeadCords = {blockX:0,blockY:0}
+
+    switch (snake.direction) {
         case "left":
-            drawBlock(snake.color,coordinateToBlock(snake.HeadBlock.x-25,snake.HeadBlock.y))
-            snake.HeadBlock = coordinateToBlock(snake.HeadBlock.x-25,snake.HeadBlock.y)
-        break;
+            newHeadCords = {blockX:snake.HeadBlock.blockX - 1, blockY:snake.HeadBlock.blockY};
+            break;
         case "right":
-            drawBlock(snake.color,coordinateToBlock(snake.HeadBlock.x+25,snake.HeadBlock.y))
-            snake.HeadBlock = coordinateToBlock(snake.HeadBlock.x+25,snake.HeadBlock.y)
-        break;
+            newHeadCords = {blockX:snake.HeadBlock.blockX + 1, blockY:snake.HeadBlock.blockY};
+            break;
         case "up":
-            drawBlock(snake.color,coordinateToBlock(snake.HeadBlock.x,snake.HeadBlock.y-25))
-            snake.HeadBlock = coordinateToBlock(snake.HeadBlock.x,snake.HeadBlock.y-25)
-        break;
+            newHeadCords = {blockX:snake.HeadBlock.blockX, blockY:snake.HeadBlock.blockY - 1};
+            break;
         case "down":
-            drawBlock(snake.color,coordinateToBlock(snake.HeadBlock.x,snake.HeadBlock.y+25))
-            snake.HeadBlock = coordinateToBlock(snake.HeadBlock.x,snake.HeadBlock.y+25)
-        break;
+            newHeadCords = {blockX:snake.HeadBlock.blockX, blockY:snake.HeadBlock.blockY + 1};
+            break;
     }
-}
-function checkGameOver(){
-    if(snake.HeadBlock.blockX > 23 || snake.HeadBlock.x < 0){
-        window.open("./index.html","_self")
-    }
-    if(snake.HeadBlock.blockY > 23 || snake.HeadBlock.y < 0){
-        window.open("./index.html","_self")
-    }
-    snake.children.forEach(element => {
-        if(element.x == snake.HeadBlock.x && element.y == snake.HeadBlock.y){
-            window.open("./index.html","_self")
-        }
-    });
-}
-function spawnPoint(start){
-    if((point.HeadBlock.x == snake.HeadBlock.x && point.HeadBlock.y == snake.HeadBlock.y) || start){
-        if(!start){
-            snake.children.push(snake.children[snake.children.length-1])
-        }
-        var b = coordinateToBlock(Math.floor((Math.random()*23)+1)*25,Math.floor((Math.random()*23)+1)*25)
-        while(snake.children.forEach(element => {
-            if(element.x != b.x && element.y != b.y){
-                return false
-            }else true
-        }) || (b.x == snake.HeadBlock.x && b.y == snake.HeadBlock.y)){
-            b = coordinateToBlock(Math.floor((Math.random() * 23)+1)*25,Math.floor((Math.random() * 23)+1)*25)
-        }
-        console.log(b)
-        point.HeadBlock = b
-        drawBlock(point.color,point.HeadBlock)
-    }
-}
-function start(){
-    document.addEventListener('keyup', (e) => {
-        if(tickPassed == true) return
-        var n = snake.direction
-        if (e.code === "ArrowUp" && n !== "down") n = "up"
-        else if (e.code === "ArrowDown" && n !== "up") n = "down"
-        else if (e.code === "ArrowLeft" && n !== "right") n = "left"
-        else if (e.code === "ArrowRight" && n !== "left") n = "right"
-        snake.direction = n
-        tickPassed = true
-      });
-    spawnPoint(true);
-    setInterval(function(){ 
-        tickPassed = false
-        moveSnake();
+    snake.HeadBlock = newHeadCords;
+    if (board[newHeadCords.blockX][newHeadCords.blockY]+2 > 3){ // This checks if the new coordinates intefere with a child
+        window.open("./index.html", "_self");
+    }else{
+        board[newHeadCords.blockX][newHeadCords.blockY] += 2; // adds 2 so in the future, the program can check whether the snake has it's head up its ass
+        drawBlock(snake.color, newHeadCords);
         spawnPoint();
+    }
+    
+}
+function spawnPoint(start) {
+    if((point.HeadBlock.blockX == snake.HeadBlock.blockX && point.HeadBlock.blockY == snake.HeadBlock.blockY) || start){
+        if(!start){
+            snake.children.push(snake.children[snake.children.length-1]) // This is what adds children to the snake, by coping the final child and duplicating it into the array.
+        }
+        var newPointCords = {blockX:Math.floor((Math.random() * 23) + 1), blockY:Math.floor((Math.random() * 23) + 1) }
+        while(board[newPointCords.blockX][newPointCords.blockY] != 0){
+            newPointCords = {blockX:Math.floor((Math.random() * 23) + 1), blockY:Math.floor((Math.random() * 23) + 1)}
+            // this generates the coordinates for the apple randomly, this isn't very efficient because in later stages of the game, it will have to loop this far more times.
+        }
+        console.log("New apple is successfully created at: ", newPointCords)
+        point.HeadBlock = newPointCords
+        board[newPointCords.blockX][newPointCords.blockY] = 1 // this declares an apple on the board
+        drawBlock(point.color, point.HeadBlock)
+    }
+}
+function start() {
+    document.addEventListener('keyup', (e) => {
+        if (tickPassed == true) return
+        var direction = snake.direction
+        if (e.code === "ArrowUp" && direction !== "down") direction = "up"
+        else if (e.code === "ArrowDown" && direction !== "up") direction = "down"
+        else if (e.code === "ArrowLeft" && direction !== "right") direction = "left"
+        else if (e.code === "ArrowRight" && direction !== "left") direction = "right"
+        snake.direction = direction 
+        tickPassed = true // this prevents the player from changing the direction in a tick.
+    });
+    spawnPoint(true);
+    setInterval(function () {
+        tickPassed = false
         checkGameOver();
     }, 125);
 }
