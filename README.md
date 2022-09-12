62f7d051152ed5c772f089ab88a47c019c4ff789
maikeru-dev <alpha12345op@gmail.com>
2022-09-12 22:03:45 +0100

    switching to classes, more concise code structure

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # snake
 I made a snake game a while ago, I decided that I should upload it.
 
+Goals:
+Game runs [x]
+Reduce number of lines and improve efficiency []
+Convert to Asynchronus(optional) []
+Smooth reloading []
+Add game over []
+Add rainbow snake []
+Remove Scroll on arrow keys []
+
+
 Here's the pages link to the game: https://maikeru-dev.github.io/snake/
--- index.js
+++ index.js
@@ -1,121 +1,162 @@
-var canvas = document.getElementById("myCanvas"); // defining essentials.
-var ctx = canvas.getContext("2d");
-
-var tickPassed = false; // tick passed is used to gatekeep changing directions after one has already been made.
-var useBrokenNumberGrid = true; // this is a useless variable.
-var board = [] // this is the board, a 2d array in the future that will allow us to quickly find if a coordinate is already occupied.
-
-var x = 0, y = 0;
-ctx.beginPath();
-ctx.font = "15px Verdana";
-
-for (var i = 0; i < 25; i++) { // this generates a grid in the canvas
-    ctx.moveTo(x, 0);
-    ctx.lineTo(x, 600);
-    ctx.moveTo(0, y);
-    ctx.lineTo(600, y);
-    y += 25
-    x += 25
-}
-ctx.stroke();
-if (useBrokenNumberGrid) { // I have no reason to keep this if statement, but it is a testament to the history of my stupidity
-    for (xAxis = 0; xAxis < 24; xAxis++) {
-        var lane = []
-        for (yAxis = 0; yAxis < 23; yAxis++) {
-            lane[yAxis] = 0 // 0 means nothing, 1 means apple, 2 means body
+class Game {
+    constructor() {
+        this.canvas = document.getElementById("myCanvas"); // defining essentials.
+        this.ctx = this.canvas.getContext("2d");
+        this.board = this.fill_board()
+        this.draw_board(this.ctx)
+        this.apple = this.spawn_apple();
+    }
+    spawn_apple() {
+        // This function is used after we figure out the snake has landed on the apple
+        let block_cordinates = { blkX: 0, blkY: 0 };
+        let valid_block = false; // unsure what to call this, defines whether we have found an empty square on the board
+        let x = 0, y = 0;
+        while (valid_block == false) {
+            x = Math.floor((Math.random() * 23) + 1);
+            y = Math.floor((Math.random() * 23) + 1);
+            if (this.board[x][y] == 0) {
+                valid_block = true;
+                block_cordinates = { blkX: x, blkY: y };
+            }
         }
-        board[xAxis] = lane
+        return block_cordinates;
     }
-}
-var snake = { HeadBlock: { blockX:12, blockY:12}, color: "#FF0000", direction: "up", children: [{ blockX: 12, blockY: 13}] }
-var point = { HeadBlock: { blockX:0, blockY:0 }, color: "#27d817" }
+    fill_board() {
+        let board = [] // this is the board, a 2d array in the future that will allow us to quickly find if a coordinate is already occupied.
 
-function drawBlock(color, block) {
-    ctx.fillStyle = color;
-    ctx.fillRect(block.blockX*25 + 1, block.blockY*25 + 1, 23, 23);
-}
-function coordinateToBlock(cx, cy) {
+        if (true) { // Load 0 into every square, this is needed for now
 
-    return { x: cx, y: cy, blockX: cx / 25, blockY: cy / 25 };
-}
-function checkGameOver() {
-    if (snake.HeadBlock.blockX > 23 || snake.HeadBlock.blockX < 0) {
-        window.open("./index.html", "_self")
-    }else if (snake.HeadBlock.blockY > 23 || snake.HeadBlock.blockY < 0) {
-        window.open("./index.html", "_self")
-    }// basic checks if the snake's head is out of bounds.
-    moveSnake(); // we chain the functions to make sure they run in order, it wouldn't matter if they were run like sub-procedures either, so this is ok.
-}
+            for (let yAxis = 0; yAxis < 24; yAxis++) {
+                board[yAxis] = []
+                for (let xAxis = 0; xAxis < 24; xAxis++) {
+                    board[yAxis][xAxis] = 0
+                }
+                // 0 means nothing, 1 means apple, 2 means body
+            }
 
-function moveSnake() {
-    const length = snake.children.length - 1 // This is the index of the last child in the array.
-
-    const cords = snake.children[length] // get the last child in the snake's children array
-    board[cords.blockX][cords.blockY] = 0 // sets the cords on the board to nothing aka 0
-    drawBlock("#fff", cords) // delete the previous child's position to remove marks
-
-    for (var i = length; i > 0; i--) { // This counts down from the array's length, starting from the index of the last child
-        snake.children[i] = snake.children[i - 1] // Example: i = 5: snake.children[5] = snake.children[4], we are shifting, the chain down and eventually the first index is invalid/duplicated.
-    }
-    snake.children[0] = snake.HeadBlock // we fix the duplication by setting the current headblock to the first child, which makes a new child and allows us to move the children, along with the head.
-
-
-    var newHeadCords = {blockX:0,blockY:0}
-
-    switch (snake.direction) {
-        case "left":
-            newHeadCords = {blockX:snake.HeadBlock.blockX - 1, blockY:snake.HeadBlock.blockY};
-            break;
-        case "right":
-            newHeadCords = {blockX:snake.HeadBlock.blockX + 1, blockY:snake.HeadBlock.blockY};
-            break;
-        case "up":
-            newHeadCords = {blockX:snake.HeadBlock.blockX, blockY:snake.HeadBlock.blockY - 1};
-            break;
-        case "down":
-            newHeadCords = {blockX:snake.HeadBlock.blockX, blockY:snake.HeadBlock.blockY + 1};
-            break;
-    }
-    snake.HeadBlock = newHeadCords;
-    if (board[newHeadCords.blockX][newHeadCords.blockY]+2 > 3){ // This checks if the new coordinates intefere with a child
-        window.open("./index.html", "_self");
-    }else{
-        board[newHeadCords.blockX][newHeadCords.blockY] += 2; // adds 2 so in the future, the program can check whether the snake has it's head up its ass
-        drawBlock(snake.color, newHeadCords);
-        spawnPoint();
-    }
-    
-}
-function spawnPoint(start) {
-    if((point.HeadBlock.blockX == snake.HeadBlock.blockX && point.HeadBlock.blockY == snake.HeadBlock.blockY) || start){
-        if(!start){
-            snake.children.push(snake.children[snake.children.length-1]) // This is what adds children to the snake, by coping the final child and duplicating it into the array.
         }
-        var newPointCords = {blockX:Math.floor((Math.random() * 23) + 1), blockY:Math.floor((Math.random() * 23) + 1) }
-        while(board[newPointCords.blockX][newPointCords.blockY] != 0){
-            newPointCords = {blockX:Math.floor((Math.random() * 23) + 1), blockY:Math.floor((Math.random() * 23) + 1)}
-            // this generates the coordinates for the apple randomly, this isn't very efficient because in later stages of the game, it will have to loop this far more times.
+        return board
+    }
+    draw_board() {
+        var x = 0, y = 0;
+        this.ctx.beginPath();
+        for (var i = 0; i < 25; i++) { // this generates a grid in the canvas
+            this.ctx.moveTo(x, 0);
+            this.ctx.lineTo(x, 600);
+            this.ctx.moveTo(0, y);
+            this.ctx.lineTo(600, y);
+            y += 25
+            x += 25
         }
-        console.log("New apple is successfully created at: ", newPointCords)
-        point.HeadBlock = newPointCords
-        board[newPointCords.blockX][newPointCords.blockY] = 1 // this declares an apple on the board
-        drawBlock(point.color, point.HeadBlock)
+        this.ctx.stroke();
+    }
+    render_block(block, color) {
+        this.ctx.fillStyle = color;
+        this.ctx.fillRect(block.blkX * 25 + 1, block.blkY * 25 + 1, 23, 23);
+    }
+
+}
+
+class Snake {
+    constructor(head_block, current_direction, snake_color) {
+        this.head_block = head_block || { blkX: 12, blkY: 12 };
+        this.color = snake_color || "#FF0000";
+        this.direction = current_direction || "up";
+        this.children = [{ blkX: 12, blkY: 13 }];
+    }
+
+    async move(game) {
+        // Adding head to children's beginning 
+        this.children.unshift({ blkX: this.head_block.blkX, blkY: this.head_block.blkY }) // Copying without reference
+
+        // On new tick, change the snake's position
+        switch (this.direction) {
+            case "up":
+                this.head_block.blkY -= 1;
+                break;
+            case "down":
+                this.head_block.blkY += 1;
+                break;
+            case "left":
+                this.head_block.blkX -= 1;
+                break;
+            case "right":
+                this.head_block.blkX += 1;
+                break;
+        }
+        let block_test = this.block_test(game)
+        if (block_test.out) return false // In case we're out of bounds
+
+        game.board[this.head_block.blkY][this.head_block.blkX] = 2 // Setting body value on board logically
+        game.render_block(this.head_block, this.color) // Renders snake's color by default, this means we're locking to one color and probably can't change it later
+
+
+        if (block_test.on_apple != true) { // In case we're not on the apple, remove the last child
+            let last_child = this.children.pop();
+            game.board[last_child.blkY][last_child.blkX] = 0
+            game.render_block(last_child, "#FFF") // Overdraw to white
+        } else {
+            game.apple = game.spawn_apple() // When we are on the apple, overwrite the current cordinates 
+        }
+    }
+    block_test(game) {
+        let response = { out: false, on_apple: false };
+
+        if (game.apple.blkX == this.head_block.blkX && game.apple.blkY == this.head_block.blkY) {
+            response.on_apple = true;
+            
+        }
+        // Check if the headblock is outside the bounds
+        if ((this.head_block.blkX > 23 || this.head_block.blkX < 0) || (this.head_block.blkY > 23 || this.head_block.blkY < 0)) {
+            response.out = true;
+            
+        }
+        if (game.board[this.head_block.blkX][this.head_block.blkY]) {
+            response.out = true
+        }
+        /*for(child in this.children){
+            if(child.blockX == head_block.blockX && child.blockY == head_block.blockY){
+                // Init Gameover
+                return true
+            }
+        }*/ // This might not even be necessary
+        return response;
     }
 }
-function start() {
-    document.addEventListener('keyup', (e) => {
+
+
+
+// Notes
+/*
+There's two systems of figuring out where the snake's body is.
+
+*/
+
+
+function main() {
+    let tickPassed = false; // tick passed is used to gatekeep changing directions after one has already been made.
+    var game = new Game()
+    var snake = new Snake()
+
+    document.addEventListener('keydown', (event) => {
+        event.preventDefault(); // Stops scrolling when using arrow keys
         if (tickPassed == true) return
         var direction = snake.direction
-        if (e.code === "ArrowUp" && direction !== "down") direction = "up"
-        else if (e.code === "ArrowDown" && direction !== "up") direction = "down"
-        else if (e.code === "ArrowLeft" && direction !== "right") direction = "left"
-        else if (e.code === "ArrowRight" && direction !== "left") direction = "right"
-        snake.direction = direction 
+        if (event.code === "ArrowUp" && direction !== "down") direction = "up"
+        else if (event.code === "ArrowDown" && direction !== "up") direction = "down"
+        else if (event.code === "ArrowLeft" && direction !== "right") direction = "left"
+        else if (event.code === "ArrowRight" && direction !== "left") direction = "right"
+        snake.direction = direction
         tickPassed = true // this prevents the player from changing the direction in a tick.
     });
-    spawnPoint(true);
-    setInterval(function () {
-        tickPassed = false
-        checkGameOver();
+
+    const gameID = setInterval(function () {
+        tickPassed = false;
+        game.render_block(game.apple, "#27d817"); // why not ?
+        let valid = snake.move(game)
+        if (valid == false) {
+            clearInterval(gameID)
+        }
+
     }, 125);
 }
